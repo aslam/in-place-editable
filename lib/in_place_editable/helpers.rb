494314f37f5edba7f61760dc9dc01113f493e1d8
August Lilleaas <augustlilleaas@gmail.com>
2009-03-23 19:18:57 +0100

    Made the plugin column type aware. Using textareas for text columns.

--- lib/in_place_editable/helpers.rb
+++ lib/in_place_editable/helpers.rb
@@ -12,10 +12,12 @@ module InPlaceEditable
       
       object_name = ActionController::RecordIdentifier.singular_class_name(instance)
       hidden_params_provider = fields_for(object_name) {|f| f.hidden_field attribute, :class => "params_provider" }
+      type = hidden_field_tag :column_type, instance.class.columns.detect {|c| c.name.to_sym == attribute }.type
       
       %{
         #{form_tag(path, {:class => "in_place_editable", :method => :put})}
           #{hidden_params_provider}
+          #{type}
           <span>#{h(instance[attribute])}</span>
         </form>
       }
